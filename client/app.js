e999000bcbb2503e302b98eb2715b5a782ee9f28
Hasn <hasnar@windowslive.com>
2017-02-21 12:12:06 +0200

    (reafactor)addToken

--- client/app.js
+++ client/app.js
@@ -30,4 +30,53 @@
 		redirectTo:'/welcome'
 	})
 	
+}).factory('Auth', function ($http, $location, $window) {
+
+  var auth = {};
+  auth.saveToken = function (token){
+    $window.localStorage['tradeworker'] = token;
+  };
+
+  auth.getToken = function (){
+    return $window.localStorage['tradeworker'];
+  };
+
+  auth.isAuth = function () {
+    return !!$window.localStorage.getItem('tradeworker');
+  };
+
+  auth.currentUser = function(){
+    if(auth.isAuth()){
+      var token = auth.getToken();
+      var payload = JSON.parse($window.atob(token.split('.')[1]));
+      return payload.username;
+    }
+  };
+
+  auth.logOut = function(){
+    $window.localStorage.removeItem('tradeworker');
+  };
+
+  auth.getProfile = function(){
+    return $http({
+      method: 'GET',
+      url: '/profile'
+    }).success(function(resp){
+      return resp;
+    }).error(function (data, status, header, config) {
+        alert('somt thing went wrong');
+    });
+  };
+
+  
+
+
+  return auth;  
 })
+.run(function ($rootScope, $location, Auth) {
+  $rootScope.$on('$routeChangeStart', function (evt, next, current) {
+    if (next.$$route && next.$$route.authenticate && !Auth.isAuth()) {
+      $location.path('/signin');
+    }
+  });
+});
--- client/app/tradeworker/Tradeworker.js
+++ client/app/tradeworker/Tradeworker.js
@@ -1,13 +1,14 @@
  angular.module('myapp.Tradeworker',[])
 
- .controller('TradeworkerContr',function ($scope,$http,$location,Tradeworker){
+ .controller('TradeworkerContr',function ($scope,$http,$location,Tradeworker, Auth){
   
   $scope.addhandworker = function(){
     console.log($scope.tradeworker);
     Tradeworker.insert($scope.tradeworker)
     .then(function (data) {
       $location.path('/welcome');
-      console.log(data);
+      console.log(data)
+      Auth.saveToken(data.token);
     })
     .catch(function (error) {
       alert(error.data.error);
