055887a371bfc22e5300f463e9912a71ae601994
Hasn <hasnar@windowslive.com>
2017-02-22 20:13:07 +0200

    (fix)signup

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var mongoose = require('mongoose');
 var server = require('http').createServer(app);//
-var port = process.env.PORT || 3000;//
+var port = process.env.PORT || 8181;//
 var app = express();
 
 
@@ -21,6 +21,6 @@ db.once('open',function () {
 
 
 ////////////////////server////////////////////////////
-app.listen(3000, function () {
+app.listen(port, function () {
   console.log(' app listening on port 3000!');//
 })
