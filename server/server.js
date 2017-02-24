2ce0b6164cf6518a9491fcff3b51a26427b25754
Hasn <hasnar@windowslive.com>
2017-02-24 22:07:13 +0200

    f

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ require('./config/middleware.js') (app,express); //
 require('./config/routes.js') (app,express); //
 
 /////////////////////database//////////////////////////
-var mongoURI = process.env.MONGODB_URI ||'mongodb://localhost/WarshahDB';
+var mongoURI = 'mongodb://hasnar:12344321s@ds161209.mlab.com:61209/warshadb'||'mongodb://localhost/WarshahDB';
   mongoose.Promise = global.Promise;
 mongoose.connect(mongoURI);
 db = mongoose.connection;
