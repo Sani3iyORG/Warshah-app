e9eef9692a19edfde33cb77b9b34e6dc49caa1f9
Hasn <hasnar@windowslive.com>
2017-02-24 22:17:58 +0200

    r

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -2,7 +2,7 @@
 var bodyParser = require('body-parser');
 
 module.exports = function (app, express) {
-  app.use(morgan('dev'));
+  //app.use(morgan('dev'));
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(express.static(__dirname + '/../../client'));
